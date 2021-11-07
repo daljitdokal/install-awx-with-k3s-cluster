87452766fdf86fa0a147eeefde7ce9b919afac65
Daljit Singh <daljit.dokal@yahoo.co.nz>
2021-11-07 15:43:43 +1300

    Update path for awx

--- install-awx.sh
+++ install-awx.sh
@@ -2,11 +2,11 @@
 sudo k3s kubectl create namespace awx
 
 # Create new `awx-operator`
-sudo k3s kubectl apply -f awx-operator.yaml -n awx
+sudo k3s kubectl apply -f awx-config/awx-operator.yaml -n awx
 sleep 120
 
 # Create `awx` with custom requirements file.
-sudo k3s kubectl apply -f awx.yaml -n awx
+sudo k3s kubectl apply -f awx-config/awx.yaml -n awx
 
 # Wait for awx to be install successfully.
 sudo k3s kubectl logs -f deployments/awx-operator -n awx
